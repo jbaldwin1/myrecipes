dabd6429ca6adc2649a49afc5f2fd73204b167c0
John <jbaldwin117@gmail.com>
2017-12-20 21:30:22 +0000

    Add edit action with tests

---
+++ test/integration/chefs_edit_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class ChefsEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create!(chefname: "jon", email: "jon@example.com",
+                         password: "password", password_confirmation: "password")
+  end              
+    
+
+  test "reject an invalid edit" do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: { chefname: " ", email: "john@email.com" } }
+    assert_template 'chefs/edit'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+  
+  test "accept valid signup" do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: { chefname: "john1 ", email: "john1@email.com" } }
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "john1", @chef.chefname
+    assert_match "john1@email.com", @chef.email
+  end
+end
+  
